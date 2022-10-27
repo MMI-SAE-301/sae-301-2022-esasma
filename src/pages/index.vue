8001a23974bf0e581b45fd6bf1ba599df39e1aa8
asma es-sabbar <asma.es-sabbar@univ-fcomte.fr>
2022-10-27 13:23:41 +0200

    ajout svg

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -8,7 +8,7 @@
     <div>
       <img class="h-44 w-full " src="@/assets/img/montre.jpg" alt="">
     </div>
-    <div>
+    <div class="bg-gris">
       <h1 class="m-3 text-center"> Tik Tok </h1>
       <p class="text-xs ml-5 mr-2"> + Tic Tac is a web site that allow you to express yourself and be yourself with
         our new
@@ -17,7 +17,18 @@
         100% made in France with quality material and recyclabe material </p>
     </div>
   </div>
-
+  <div class="grid grid-cols-2 mt-10">
+    <div>
+      <img class="h-72 w-64 " src="@/assets/img/daniel-korpai-hbTKIbuMmBI-unsplash 1.jpg" alt="">
+    </div>
+    <div class>
+      <p class="m-3 text-center"> 159,99 $</p>
+      <p class="text-xs ml-5 mr-2"> A symbol of uniqueness and rarity, the Tik Tak watch is distinguished by its
+        minimalist design and its many variations. <br />
+        Only priced at $159.00, it guarantees unique comfort. <br />
+        100% made in France with quality material and recyclabe material </p>
+    </div>
+  </div>
   <h2 class="text-center m-6"> Tik Tok </h2>
   <div class="grid grid-cols-4 "> <img src="@/assets/img/montre4.jpg" alt="">
     <img src="@/assets/img/montre3.png" alt="">
@@ -29,9 +40,27 @@
     <Button />
   </div>
 
-
+  <div class="flex grid-flow-row bg-gris p-2 m-10">
+    <div class="p-1">
+      <photoavis />
+      <p> Elise F****</p>
+      <p> Octobre 22, 2022 </p>
+    </div>
+    <div class="grid">
+      <div class="flex">
+        <etoileVue />
+        <etoileVue />
+        <etoileVue />
+        <etoileVue />
+        <etoileVue />
+      </div>
+      <p> The watch is perfect, the materials are great </p>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import Button from "@/components/Boutton.vue"
+import etoileVue from "@/components/etoile.vue";
+import photoavis from "@/components/photoavis.vue"
 </script>
